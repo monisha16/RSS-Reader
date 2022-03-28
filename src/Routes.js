508af5fceadb9a6f15c12590be4532a48e8084b4
Monisha Tarkar <monishatarkar1625@gmail.com>
2022-03-28 16:23:21 +0530

    hndling routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,18 @@
 import React from "react";
-// import styled from "styled-components";
 import { Routes, Route } from 'react-router-dom';
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Mainpage from "./components/Mainpage/Mainpage";
 import FeedView from "./components/FeedView/FeedView";
 import NotFound from "./components/NotFound/NotFound";
 
-//Handle Indications of which page we are at
+
 const RoutesComp = () => (
   
     <Routes>
       <Route exact path="/" element={<Mainpage />} />
       <Route exact path="/:feedid" element={<FeedView />} />
       <Route exact path="/bookmarks" element={<Bookmarks />} />
-      <Route element={<NotFound />} />
+      <Route path="/bookmarks/*" element={<NotFound />} />
     </Routes>
 
     
